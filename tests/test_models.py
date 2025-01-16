10c577dc502c94dcec7a6845225f7d96c49af57c
Steven C Chan <80276802+SCChan21@users.noreply.github.com>
2025-01-16 11:26:53 +0000

    test-suite

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max, daily_min
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +29,25 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_arange():
+
+    # test_input = np.arange(6).reshape(3, 2)
+    test_input = np.array([[0, 1],
+                           [2, 3],
+                           [4, 5]])
+    test_result = np.array([4, 5])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_arange():
+
+    # test_input = np.arange(6).reshape(3, 2)
+    test_input = np.array([[0, 1],
+                           [2, 3],
+                           [4, 5]])
+    test_result = np.array([0, 1])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
