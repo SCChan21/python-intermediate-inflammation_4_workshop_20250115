8548894b6784ea901c4f1d673be3f5755c12ff31
Steven C Chan <80276802+SCChan21@users.noreply.github.com>
2025-01-17 15:09:12 +0000

    Resolve conflicts?

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -36,9 +36,9 @@ def main(args):
         inflammation_data = models.load_csv(filename)
 
         view_data = {
-            'average': models.daily_mean(inflammation_data),
-            'max': models.daily_max(inflammation_data),
-            'min': models.daily_min(inflammation_data),
+            "average": models.daily_mean(inflammation_data),
+            "max": models.daily_max(inflammation_data),
+            "min": models.daily_min(inflammation_data),
         }
 
         views.visualize(view_data)
